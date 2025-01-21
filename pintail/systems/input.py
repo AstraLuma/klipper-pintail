1421c1a384caea078476ab7b79029f6acf6abf39
Jamie Bliss <jamie@ivyleav.es>
2025-01-21 15:20:21 -0500

    Move input handling to a reader thread
    Idle now only happens when something else incidentally happens, so needs
    to be moved

--- pintail/systems/input.py
+++ pintail/systems/input.py
@@ -2,6 +2,8 @@
 Accept input from the display knob
 """
 import logging
+import selectors
+import threading
 
 import evdev
 from evdev import ecodes
@@ -16,23 +18,42 @@ class Input(ppb.systemslib.System):
         "/dev/input/by-path/platform-button@c-event",
         "/dev/input/by-path/platform-rotary@13-event",
     ]
+
+    _thread = None
+    _selector = None
+
     def __enter__(self):
-        self.devices = [
+        self._thread = threading.Thread(None, self._read_thread, name=f"evdev-reader", daemon=True)
+        self._thread.start()
+
+
+    def __exit__(self, *exc):
+        if self._selector is not None:
+            self._selector.close()
+
+    def _read_thread(self):
+        self._selector = selectors.DefaultSelector()
+
+        devices = [
             evdev.InputDevice(path)
             for path in self.input_devices
         ]
 
-    def __exit__(self, *exc):
-        for dev in self.devices:
-            dev.close()
+        for dev in devices:
+            self._selector.register(dev, selectors.EVENT_READ)
 
-    def on_idle(self, event, signal):
-        for dev in self.devices:
-            for ev in iter(dev.read_one, None):
-                if ev.type == ecodes.EV_KEY and ev.code == ecodes.BTN_0:
-                    if ev.value:
-                        signal(events.KnobPress())
-                    else:
-                        signal(events.KnobRelease())
-                elif ev.type == ecodes.EV_REL and ev.code == ecodes.REL_X:
-                    signal(events.KnobTurn(direction=events.Direction(ev.value)))
+        while True:
+            for key, mask in self._selector.select():
+                device = key.fileobj
+                for event in device.read():
+                    self._decode(event)
+
+
+    def _decode(self, ev):
+        if ev.type == ecodes.EV_KEY and ev.code == ecodes.BTN_0:
+            if ev.value:
+                self.engine.signal(events.KnobPress())
+            else:
+                self.engine.signal(events.KnobRelease())
+        elif ev.type == ecodes.EV_REL and ev.code == ecodes.REL_X:
+            self.engine.signal(events.KnobTurn(direction=events.Direction(ev.value)))
